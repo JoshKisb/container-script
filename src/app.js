82fc42dc9df4f84cde0b35b9e597be133298e804
Joshua Kisb <joshuakisb@gmail.com>
2024-07-09 09:42:46 +0300

    update script

--- src/app.js
+++ src/app.js
@@ -37,9 +37,15 @@ app.post('/stop-report', async (req, res) => {
 app.post('/run-update', async (req, res) => {
     try {
         // Run script 'insert.js'
-        const { stdout, stderr } = await exec('lxc exec layer -- node app/insert.js');
-        console.log('Running insert script:', stdout);
-        res.status(200).send('Script executed successfully.');
+        const { stdout, stderr, error } = await execShellCommand('lxc exec layer -- node icyd-app/insert.js');
+        console.log('Running insert script, stdout:', stdout);
+        console.log('Running insert script, stderr:', stderr);
+        if (error) {
+            console.error('Error running insert script:', error);
+            res.status(500).send('Failed to execute script.');
+        } else {
+            res.status(200).send('Script executed successfully.');
+        }
     } catch (e) {
         console.error('Error running insert script:', e);
         res.status(500).send('Failed to execute script.');
