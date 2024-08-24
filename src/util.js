9d5055650c91378b42e4156df2417bfc79081f26
Joshua Kisb <joshuakisb@gmail.com>
2024-08-24 19:38:09 +0300

    4000 records

--- src/util.js
+++ src/util.js
@@ -48,7 +48,7 @@ const generateCSV = async () => {
     const result = await client.query(`
       SELECT x.* 
       FROM public.program_instance_base_view x
-      LIMIT 2000
+      LIMIT 4000
     `);
 
     // Extract headers from result.fields
